3482d3398d4845539c1eaa0137921727a28af93c
Антон Косых <109937068+Akakiiy@users.noreply.github.com>
2023-06-04 12:42:46 +0300

    починка авторизации

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,12 @@ import {
 
 export const debugMode = false
 
+// export const tokenAtom = atom('6126e8c08dbcb7cef6e4294b9eb76df6e46dc769')
 export const tokenAtom = atom(localStorage.getItem('token') || '')
 
 const App: React.FC = () => {
   const token = useAtomValue(tokenAtom)
+  console.log(token)
 
   return (
     <div className="app">
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,18 @@
 import { axiosInstance } from './abstract'
+import axios from 'axios'
 
 export const authService = {
   /*
   * Авторизация на сервере
   * */
+  // login: (username: string, password: string) => {
+  //   return axiosInstance.post('/auth/token/login', {
+  //     username,
+  //     password
+  //   })
+  // }
   login: (username: string, password: string) => {
-    return axiosInstance.post('/auth/token/login/', {
+    return axios.post('http://myhectare.ru:8000/api/v1/auth/token/login', {
       username,
       password
     })
