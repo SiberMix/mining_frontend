0f26abb2025049d39135c3996682b153b0b8789c
Антон Косых <109937068+Akakiiy@users.noreply.github.com>
2023-06-16 13:22:09 +0300

    исправлен баг с удалением полигонов

--- src/redux/slices/mapSlice.tsx
+++ src/redux/slices/mapSlice.tsx
@@ -103,6 +103,9 @@ const mapSlice = createSlice({
         state.drawingPolygonMode = false
         state.editedPolygon = undefined
       })
+      .addCase(deletePolygon.fulfilled, (state: MapInitialState, action) => {
+        state.polygonsList = state.polygonsList.filter(polygon => polygon.id !== action.payload.id)
+      })
       .addCase(postNewEquipment.fulfilled, (state: MapInitialState, action) => {
         //todo мне нужно чтоб сервер возвращал equipment с id
         // state.equipmentList = [...state.equipmentList, action.payload.data]
