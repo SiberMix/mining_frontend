3d15e05cbc01c02cc522b139bba6b5caa7ff397e
Антон Косых <spinsib@gmail.com>
2023-08-05 08:57:57 +0300

    добавил чарт в превью полигонов

--- src/components/pages/Analytic/modules/CropRotation/CropRotationList/PolygonDrawerPreview/PolygonDrawerPreview.tsx
+++ src/components/pages/Analytic/modules/CropRotation/CropRotationList/PolygonDrawerPreview/PolygonDrawerPreview.tsx
@@ -5,6 +5,8 @@ import { Drawer } from 'antd'
 import MiniMap from '../../../../../../common/MiniMap/MiniMap'
 import { useSelector } from 'react-redux'
 import { getAllPolygonsSelector } from '../../../../../../../redux/selectors/mapSelectors'
+import Chart from 'react-apexcharts'
+import { ApexOptions } from 'apexcharts'
 
 type Props = {
   isOpen: boolean
@@ -18,6 +20,57 @@ const PolygonDrawerPreview: React.FC<Props> = ({
   isOpen
 }) => {
   const allPolygons = useSelector(getAllPolygonsSelector)
+
+  const options: ApexOptions = {
+    chart: {
+      type: 'radialBar',
+      offsetY: -20,
+      sparkline: {
+        enabled: true
+      }
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -90,
+        endAngle: 90,
+        track: {
+          background: '#e7e7e7',
+          strokeWidth: '97%',
+          margin: 5, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            color: '#999',
+            opacity: 1,
+            blur: 2
+          }
+        },
+        dataLabels: {
+          name: {
+            show: false
+          },
+          value: {
+            offsetY: -2,
+            fontSize: '19px',
+            color: '#ffffff'
+          }
+        }
+      }
+    },
+    colors: [polygon.sequence.color],
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'light',
+        shadeIntensity: 0.4,
+        inverseColors: false,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 50, 53, 91]
+      }
+    }
+  }
 
   const polygonsWithSameSequence = allPolygons.filter(p => p.sequence.name === polygon.sequence.name)
   const squareOfAllSamePolygons = polygonsWithSameSequence.reduce((acc: number, cur: PolygonType) => {
@@ -53,10 +106,16 @@ const PolygonDrawerPreview: React.FC<Props> = ({
         <span className='polygonDrawerPreview-info_name'>Общая площадь: </span>
         <span className='polygonDrawerPreview-info'>{polygon.square} га</span>
       </p>
-      <p>
+      <div className='polygonDrawerPreview-info_chart'>
         <span className='polygonDrawerPreview-info_name'>Процент от всей культуры: </span>
-        <span className='polygonDrawerPreview-info'>{percentOfSequence} %</span>
-      </p>
+        <Chart
+          options={options}
+          type='radialBar'
+          series={[+percentOfSequence]}
+          width='100%'
+          height={315}
+        />
+      </div>
     </Drawer>
   )
 }
